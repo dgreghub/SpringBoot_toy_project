08c99998a7a5709730349f9338885cd61dfdc64a
dgreg.shin <dgreg.shin@gmail.com>
2021-12-13 00:11:37 +0900

    [v0.1] 기본 테스트 코드 작성연습 - 등록 API 생성하기

--- src/test/java/com/springboot/toy/web/PostsApiControllerTest.java
+++ src/test/java/com/springboot/toy/web/PostsApiControllerTest.java
@@ -1,4 +1,41 @@
 package com.springboot.toy.web;
 
+import com.springboot.toy.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @After
+    public void tearDown() throws Exception{
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() throws Exception{
+
+    }
 }
