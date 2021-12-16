7446ccbc143b71f0d9e648fab7bb6faecbf55ab5
dgreg.shin <dgreg.shin@gmail.com>
2021-12-17 00:03:25 +0900

    [v0.1] 기본 테스트 코드 작성연습 - JPA Auditing 적용

--- src/main/java/com/springboot/toy/domain/posts/Posts.java
+++ src/main/java/com/springboot/toy/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.springboot.toy.domain.posts;
 
+import com.springboot.toy.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter //클래스 내 모든 필드의 Getter 메소드를 자동으로 생성
 @NoArgsConstructor //기본생성자 자동 추가
 @Entity //테이블과 링크될 클래스임을 나타냄 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭함.
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id // 해당 테이블의 PK 필드를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙을 나타냄
