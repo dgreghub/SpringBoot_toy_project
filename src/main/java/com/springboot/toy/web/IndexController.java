4a37a029e3a0c27b53d8b325dac860356164a27e
dgreg.shin <dgreg.shin@gmail.com>
2021-12-24 03:13:30 +0900

    [v0.1] 기본 테스트 코드 작성연습 - Mustache를 활용한 전체조회 화면 만들기

--- src/main/java/com/springboot/toy/web/IndexController.java
+++ src/main/java/com/springboot/toy/web/IndexController.java
@@ -1,14 +1,22 @@
 package com.springboot.toy.web;
 
+import com.springboot.toy.service.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
     //index를 반환하므로 index.mustache로 전환되어 View Resolver 가 처리하게 된다.
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
         return "index";
     }
 
