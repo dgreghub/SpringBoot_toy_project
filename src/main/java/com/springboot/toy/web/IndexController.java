81c3397eae9606d22e7ca85adc551f858d1e5441
dgreg.shin <dgreg.shin@gmail.com>
2021-12-25 01:28:13 +0900

    [v0.1] 기본 테스트 코드 작성연습 - 수정 API 만들기

--- src/main/java/com/springboot/toy/web/IndexController.java
+++ src/main/java/com/springboot/toy/web/IndexController.java
@@ -1,10 +1,15 @@
 package com.springboot.toy.web;
 
 import com.springboot.toy.service.PostsService;
+import com.springboot.toy.web.dto.PostsResponseDto;
+import com.springboot.toy.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,4 +29,13 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
+    }
+
 }
