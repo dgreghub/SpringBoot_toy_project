5d8a84f33924180ba1658ad46190a122bbde8b6c
dgreg.shin <dgreg.shin@gmail.com>
2022-01-18 09:03:54 +0900

    [v0.1] 기본 테스트 코드 작성연습 - Nginx 리버스 프로시 설정을 위한 8081 8082 설정 확인

--- src/main/java/com/springboot/toy/config/auth/SecurityConfig.java
+++ src/main/java/com/springboot/toy/config/auth/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     protected  void configure(HttpSecurity http) throws Exception{
         http.csrf().disable().headers().frameOptions().disable() //h2-console 화면을 사용하기 위해 해당 옵션들을 disable 합니다.
                 .and().authorizeRequests() //URL별 권한 관리를 설정하는 옵션의 시작점 authorizeRequests 선언되어야만 antMatchers 옵션을 사용할 수 있다.
-                .antMatchers("/","/css/**","/images/**","/js/**","h2-console/**").permitAll()
+                .antMatchers("/","/css/**","/images/**","/js/**","h2-console/**","/profile").permitAll()
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //URL, HTTP 메소드별로 관리가 가능하다. /api/vi/** 주소를 가진 API는 USER만 열람 가능하다.
                 .anyRequest().authenticated()//설정된 값들 이외 나머지 URL들은 나타냅니다. authenticated()을 추가하여 나머지 URL 들은 모두 인증된 사용자들에게만 허용됨
                 .and().logout().logoutSuccessUrl("/")// 로그아웃 기능에 대한 여러 설정의 진입점이다. 로그아웃 성공시 "/" 주소로 향한다.
